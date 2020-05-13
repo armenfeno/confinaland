6c4726f2a0858c22651640b7b3e60b9d040446ef
Armengol13 <armengol.xavi@gmail.com>
2020-05-13 16:55:40 +0200

    Added new Confinaland Key

---
+++ src/main/java/net/mcreator/confinalandroleplay/item/ItemConfinalandKey.java
@@ -0,0 +1,77 @@
+
+package net.mcreator.confinalandroleplay.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.confinalandroleplay.ElementsConfinalandRoleplay;
+
+import java.util.List;
+
+@ElementsConfinalandRoleplay.ModElement.Tag
+public class ItemConfinalandKey extends ElementsConfinalandRoleplay.ModElement {
+	@GameRegistry.ObjectHolder("confinalandroleplay:confinalandkey")
+	public static final Item block = null;
+	public ItemConfinalandKey(ElementsConfinalandRoleplay instance) {
+		super(instance, 22);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("confinalandroleplay:confinalandkey", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 1;
+			setUnlocalizedName("confinalandkey");
+			setRegistryName("confinalandkey");
+			setCreativeTab(CreativeTabs.TOOLS);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<String> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add("Llave del pueblo de Confinaland");
+			list.add("Propiedad del Alcalde");
+		}
+	}
+}
