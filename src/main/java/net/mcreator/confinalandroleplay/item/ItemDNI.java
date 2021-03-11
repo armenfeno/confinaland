e3d7572e9bf6c7074be6472e8fa1cc576035dda7
Armengol13 <Armengol13@DESKTOP-3FSFD5Q.lan>
2021-03-11 20:13:36 +0100

    DNI añadido

---
+++ src/main/java/net/mcreator/confinalandroleplay/item/ItemDNI.java
@@ -0,0 +1,70 @@
+
+package net.mcreator.confinalandroleplay.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import net.mcreator.confinalandroleplay.ElementsConfinalandRoleplay;
+
+import java.util.List;
+
+@ElementsConfinalandRoleplay.ModElement.Tag
+public class ItemDNI extends ElementsConfinalandRoleplay.ModElement {
+	@GameRegistry.ObjectHolder("confinalandroleplay:dni")
+	public static final Item block = null;
+	public ItemDNI(ElementsConfinalandRoleplay instance) {
+		super(instance, 73);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("confinalandroleplay:dni", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("dni");
+			setRegistryName("dni");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<String> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add("DNI Confinaliense");
+		}
+	}
+}
